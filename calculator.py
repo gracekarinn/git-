77229c5eca6bb91b790e2c72794f8b9304d07c75
narawaa <nashwa.ghania@ui.ac.id>
2026-02-05 08:38:23 +0700

    feat: subtract

--- calculator.py
+++ calculator.py
@@ -1,8 +1,8 @@
 class Calculator:
-    def add(self, a, b):
-        return a + b
+    def subtract(self, a, b):
+        return a - b
     
     
 if __name__ == "__main__":
     calc = Calculator()
-    print("Addition: ", calc.add(10, 5))
+    print("Subtraction: ", calc.subtract(10, 5))
